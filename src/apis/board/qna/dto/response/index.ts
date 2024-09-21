b8769b1276b377ede6808702fd6a67c1c13be4d2
hhhunnn <gusdk2090@naver.com>
2024-09-21 23:39:42 +0900

    qna category

--- src/apis/board/qna/dto/response/index.ts
+++ src/apis/board/qna/dto/response/index.ts
@@ -12,6 +12,11 @@ export interface GetSearchQnaListResponseDto extends ResponseDto {
     qnaList: QnaListItem[];
 };
 
+// description: 문의사항 카테고리 필터 리스트 불러오기 Response Body DTO
+export interface GetQnaCategoryListResponseDto extends ResponseDto {
+    qnaList: QnaListItem[];
+};
+
 export interface GetQnaDetailResponseDto extends ResponseDto {
     qnaNumber: number;
     status: boolean;
