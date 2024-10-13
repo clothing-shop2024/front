9805fd76050b8e7de79c47bff8da59f17b6a4eca
hhhunnn <gusdk2090@naver.com>
2024-10-13 23:39:19 +0900

    cloth-detail main page 수정중

---
+++ src/apis/clothDetail/dto/response/index.ts
@@ -0,0 +1,12 @@
+import ResponseDto from "src/apis/response.dto";
+import { ClothDetailListItem } from "src/types";
+
+// description: 옷 상세 테이블 전체 리스트 불러오기 Response Body DTO
+export interface GetClothDetailListResponseDto extends ResponseDto {
+    clothDetailList: ClothDetailListItem;
+}
+
+// description: 옷 상세 테이블 전체 리스트 불러오기 Response Body DTO
+export interface GetBestClothDetailListResponseDto extends ResponseDto {
+    bestClothDetailList: ClothDetailListItem;
+}
