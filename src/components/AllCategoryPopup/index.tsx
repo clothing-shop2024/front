6f854cdc0ac0b0e110f40b90796c17c2c133b935
hhhunnn <gusdk2090@naver.com>
2024-10-21 23:39:37 +0900

    side-bar-navigation

--- src/components/AllCategoryPopup/index.tsx
+++ src/components/AllCategoryPopup/index.tsx
@@ -1,6 +1,6 @@
 import { useCookies } from "react-cookie";
 import { useLocation, useNavigate } from "react-router";
-import { FAQ_LIST_ABSOLUTE_PATH, MAIN_ABSOLUTE_PATH, NOTICE_LIST_ABSOLUTE_PATH, QNA_LIST_ABSOLUTE_PATH, SIGN_IN_ABSOLUTE_PATH, SIGN_UP_ABSOLUTE_PATH } from "src/constant";
+import { CLOTH_DETAIL_CATEGORY1_LIST_ABSOLUTE_PATH, FAQ_LIST_ABSOLUTE_PATH, MAIN_ABSOLUTE_PATH, NOTICE_LIST_ABSOLUTE_PATH, QNA_LIST_ABSOLUTE_PATH, SIGN_IN_ABSOLUTE_PATH, SIGN_UP_ABSOLUTE_PATH } from "src/constant";
 import useUserStore from "src/stores/user.store";
 import './style.css';
 
@@ -19,10 +19,19 @@ export const AllCategoryPopup = ({ onClose }: { onClose: () => void }) => {
     const notLoginInState = loginUserRole !== 'ROLE_USER' && loginUserRole !== 'ROLE_ADMIN';
     const loginInState = loginUserRole === 'ROLE_USER' || loginUserRole === 'ROLE_ADMIN';
 
+    const onOuterClickHandler = () => navigator(CLOTH_DETAIL_CATEGORY1_LIST_ABSOLUTE_PATH('OUTER'));
+
+    const onTopClickHandler = () => navigator(CLOTH_DETAIL_CATEGORY1_LIST_ABSOLUTE_PATH('TOP'));
+
+    const onBottomClickHandler = () => navigator(CLOTH_DETAIL_CATEGORY1_LIST_ABSOLUTE_PATH('BOTTOM'));
+    
+    const onOpsClickHandler = () => navigator(CLOTH_DETAIL_CATEGORY1_LIST_ABSOLUTE_PATH('OPS/SKIRT'));
+
+    const onAccClickHandler = () => navigator(CLOTH_DETAIL_CATEGORY1_LIST_ABSOLUTE_PATH('ACC'));
+
     const onNoticeClickHandler = () => {
         onClose();
         navigator(NOTICE_LIST_ABSOLUTE_PATH);
-        
     }
     const onQnaClickHandler = () => {
         onClose();
@@ -32,7 +41,6 @@ export const AllCategoryPopup = ({ onClose }: { onClose: () => void }) => {
         onClose();
         navigator(FAQ_LIST_ABSOLUTE_PATH);
     }
-
 
     const onSignInClickHandler = () => {
         onClose();
@@ -65,34 +73,56 @@ export const AllCategoryPopup = ({ onClose }: { onClose: () => void }) => {
                 <div className='close-all-category' onClick={onClose}>x</div>
 
                 <div className='all-cloth-category-detail'>
-                    <div className='side-bar-button'>BEST</div>
-                    <div className='side-bar-button'>OUTER</div>
-                    <div className='side-bar-button'>TOP</div>
-                    <div className='side-bar-button'>BOTTOM</div>
-                    <div className='side-bar-button'>DRESS & SKIRT</div>
-                    <div className='side-bar-button'>BAG & SHOES</div>
-                    <div className='side-bar-button'>ACC</div>
+                    <div className='side-bar-button-container'>
+                        <div className='side-bar-button' onClick={onOuterClickHandler}>OUTER</div>
+                    </div>
+                    <div className='side-bar-button-container'>
+                        <div className='side-bar-button' onClick={onTopClickHandler}>TOP</div>
+                    </div>
+                    <div className='side-bar-button-container'>
+                        <div className='side-bar-button' onClick={onBottomClickHandler}>BOTTOM</div>
+                    </div>
+                    <div className='side-bar-button-container'>
+                        <div className='side-bar-button' onClick={onOpsClickHandler}>OPS/SKIRT</div>
+                    </div>
+                    <div className='side-bar-button-container'>
+                        <div className='side-bar-button' onClick={onAccClickHandler}>ACC</div>
+                    </div>
                 </div>
 
                 <div className='all-board-category'>
-                    <div className='side-bar-button' onClick={onNoticeClickHandler}>공지사항</div>
-                    <div className='side-bar-button' onClick={onQnaClickHandler}>문의사항</div>
-                    <div className='side-bar-button' onClick={onFaqClickHandler}>자주하는질문(FAQ)</div>
+                    <div className='side-bar-button-container'>
+                        <div className='side-bar-button' onClick={onNoticeClickHandler}>공지사항</div>
+                    </div>
+                    <div className='side-bar-button-container'>
+                        <div className='side-bar-button' onClick={onQnaClickHandler}>문의사항</div>
+                    </div>
+                    <div className='side-bar-button-container'>
+                        <div className='side-bar-button' onClick={onFaqClickHandler}>자주하는질문(FAQ)</div>
+                    </div>
                 </div>
 
-                <div className='side-bar-button'>
-                    {notLoginInState &&
-                        <div className='sign-in-button' onClick={onSignInClickHandler}>로그인</div>
-                    }
-                    {notLoginInState &&
-                        <div className='side-bar-button' onClick={onSignUpClickHandler}>회원가입</div>
+                <div className='side-bar-login-container'>
+                    {notLoginInState && 
+                    <>
+                        <div className='side-bar-button-container'>
+                            <div className='side-bar-button' onClick={onSignInClickHandler}>로그인</div>
+                        </div>
+                        <div className='side-bar-button-container'>
+                            <div className='side-bar-button' onClick={onSignUpClickHandler}>회원가입</div>
+                        </div>
+                    </>
                     }
 
                     {loginInState &&
-                        <div className='side-bar-button' >주문조회</div>
-                    }
-                    {loginInState &&
-                        <div className='side-bar-button' onClick={onLogOutClickHandler}>로그아웃</div>
+                        <>
+                            <div className='side-bar-button-container'>
+                                <div className='side-bar-button' >주문조회</div>
+                            </div>
+                            <div className='side-bar-button-container'>
+                                <div className='side-bar-button' onClick={onLogOutClickHandler}>로그아웃</div>
+                            </div>
+                        </>
                     }
                 </div>
             </div>
